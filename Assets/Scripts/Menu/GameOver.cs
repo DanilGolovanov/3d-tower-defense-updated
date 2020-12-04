0856b2d25666c2b73be5e8aef846e4f05a024452
mdk-1 <mdkin2006@gmail.com>
2020-12-04 12:54:52 +1100

    win/lose condition scenes added
    adding win/loss scenes

---
+++ Assets/Scripts/Menu/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//script to handle gameover menu
+//adds public methods to add to onclick events for UI buttons
+
+public class GameOver : MonoBehaviour
+{
+    //exit method
+    //if in editor, exit
+    //otherwise exit application
+    public void ExitToDesktop()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+    //load scene by string
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
